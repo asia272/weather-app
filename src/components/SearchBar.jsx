d43aab0492addf9fdaceaadd85fdddd0e6a72fdb
Asia Ashraf <asiaashraf7272@gmail.com>
2025-09-20 18:33:14 +0500

    add seach icon

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-
 import SearchInProgress from "./SearchInProgress";
 import "../styles/SearchBar.css"
-
+import searchIcon from "../assets/images/icon-search.svg";
 
 
 export default function SearchBar({ onSearch, searching }) {
@@ -20,13 +19,17 @@ export default function SearchBar({ onSearch, searching }) {
       <h1 data-aos="fade-down">How's the sky looking today?</h1>
       <form className="search-bar-form" onSubmit={handleSubmit}>
         <div className="inputs">
-          <input
-            type="text"
-            placeholder="Search for a place..."
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            data-aos="fade-right"
-          />
+          <div className="input-icon">
+            <img src={searchIcon} alt="search-icon" />
+            <input
+              type="text"
+              placeholder="Search for a place..."
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              data-aos="fade-right"
+            />
+          </div>
+
           {searching && (
             <SearchInProgress />
           )}
