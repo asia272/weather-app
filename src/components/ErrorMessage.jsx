c60742ba8ef96b008f73004a8ea36bed54c728ac
Asia Ashraf <asiaashraf7272@gmail.com>
2025-09-21 19:20:40 +0500

    retry icon added

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/ErrorMessage.css";
 import erroImg from "../assets/images/icon-error.svg"
-
+import retryIcon from "../assets/images/icon-retry.svg"
 function ErrorMessage({ message, onRetry }) {
     return (
         <section className="error-container">
@@ -12,6 +12,7 @@ function ErrorMessage({ message, onRetry }) {
                 <h2>Something went wrong</h2>
                 <p>{message || "We couldn’t connect to the server (API error). Please try again in a few moments."}</p>
                 <button className="retry-btn" onClick={onRetry}>
+                    <img src={retryIcon} alt="" />
                     Retry
                 </button>
             </div>
