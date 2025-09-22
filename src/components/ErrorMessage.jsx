0e6373995854d58d59e3ceeb530e0446beaf079d
Asia Ashraf <asiaashraf7272@gmail.com>
2025-09-22 13:30:52 +0500

    accessbilty added in error msg componetn

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import "../styles/ErrorMessage.css";
-import erroImg from "../assets/images/icon-error.svg"
-import retryIcon from "../assets/images/icon-retry.svg"
+import erroImg from "../assets/images/icon-error.svg";
+import retryIcon from "../assets/images/icon-retry.svg";
+
 function ErrorMessage({ message, onRetry }) {
     return (
-        <section className="error-container">
-            <div className="error-message ">
-                <div className="error-icon">
-                    <img src={erroImg} alt="error-icon" />
-                </div>
-                <h2>Something went wrong</h2>
-                <p>{message || "We couldn’t connect to the server (API error). Please try again in a few moments."}</p>
+        <section className="error-container" role="alert">
+            <article className="error-message">
+                <figure className="error-icon">
+                    <img src={erroImg} alt="Error icon" />
+                </figure>
+
+                <header>
+                    <h2>Something went wrong</h2>
+                </header>
+
+                <p>
+                    {message ||
+                        "We couldn’t connect to the server (API error). Please try again in a few moments."}
+                </p>
+
                 <button className="retry-btn" onClick={onRetry}>
-                    <img src={retryIcon} alt="" />
+                    <img src={retryIcon} alt="Retry icon" />
                     Retry
                 </button>
-            </div>
+            </article>
         </section>
-
     );
 }
 
